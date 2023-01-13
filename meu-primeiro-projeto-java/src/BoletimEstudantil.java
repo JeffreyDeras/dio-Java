8e6ac79a53e6e516e3259533b09d984ea1104e3f
JeffreyDeras <jefreyjb10@gmail.com>
2023-01-13 17:24:29 -0300

    Inclusão-Sintaxe

---
+++ meu-primeiro-projeto-java/src/BoletimEstudantil.java
@@ -0,0 +1,15 @@
+import javax.lang.model.util.ElementScanner14;
+
+/*Referente a aula da DIO sobre Sintaxe Java*/
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 5;
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (mediaFinal == 6)
+            System.out.println("Prova minerva");
+        else
+            System.out.println("Aprovado");
+    }
+}
