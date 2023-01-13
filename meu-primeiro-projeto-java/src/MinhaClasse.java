d2b05be047c5d4cddaeebf919878b1a8fa2cd1e0
JeffreyDeras <jefreyjb10@gmail.com>
2023-01-13 15:53:31 -0300

    1301203 metodo simple

--- meu-primeiro-projeto-java/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -1,9 +1,22 @@
+import java.lang.invoke.StringConcatException;
+
+import javax.print.DocFlavor.STRING;
+
 public class MinhaClasse {
 
     public static void main(String[] args) {
 
+        String primeiroNome = "Jeffrey";
+        String segundoNome ="Barbosa";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
         
-        System.out.println();
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+        
+    }
     }
     
-}
