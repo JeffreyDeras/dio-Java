7650fbaf402d08f5404e16be38863c6d49b89e6c
JeffreyDeras <jefreyjb10@gmail.com>
2023-01-30 16:45:50 -0300

    30012023

---
+++ meu-primeiro-projeto-java/src/anatomia-das-classes/Condicional.java
@@ -0,0 +1,16 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 10;
+        b = 10;
+
+        String resultado = a==b?"Verdadeiro" : "False";
+
+        System.out.println(resultado);
+
+    
+        
+
+    }
+}
---
+++ meu-primeiro-projeto-java/src/anatomia-das-classes/Relacionais.java
@@ -0,0 +1,14 @@
+public class Relacionais {
+    public static void main(String[] args) {
+        
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+
+
+        System.out.println(" Numero1 é igual a Numero2? " +  simNao); 
+        
+    }
+}
